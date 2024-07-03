918515ab672cad996ca977b31fb26709d34f21d6
whats2000 <60466660+whats2000@users.noreply.github.com>
2024-07-03 14:21:29 +0800

    Fix: Fix missing base url

--- nsysu_selector_helper/vite.config.ts
+++ nsysu_selector_helper/vite.config.ts
@@ -1,18 +1,19 @@
-import {resolve} from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    plugins: [
-      ['@swc/plugin-styled-components', { displayName: false }],
-    ],
-  })],
+  plugins: [
+    react({
+      plugins: [['@swc/plugin-styled-components', { displayName: false }]],
+    }),
+  ],
   resolve: {
     alias: [
-      {find: '@', replacement: resolve(__dirname, 'src')},
-      {find: '#', replacement: resolve(__dirname, 'src/components')},
+      { find: '@', replacement: resolve(__dirname, 'src') },
+      { find: '#', replacement: resolve(__dirname, 'src/components') },
     ],
   },
-})
+  base: '/selector_helper_react/',
+});
