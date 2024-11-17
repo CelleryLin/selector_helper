2e15a07e81bc54374b9fede05ab460fb13b8ef30
whats2000 <60466660+whats2000@users.noreply.github.com>
2024-11-18 04:22:33 +0800

    Add: Add new API connector

--- client-website/src/types/index.d.ts
+++ client-website/src/types/index.d.ts
@@ -1,5 +1,6 @@
 export type * from './Course';
 export type * from './CouseDataFilesInfo';
 export type * from './Filter';
+export type * from './NSYSUCourseAPI';
 export type * from './Time';
 export type * from './TimeSlot';
